85873ceb5945df76d49d47dc487538529a7294bb
shkhkmt <93771404+shkhkmt@users.noreply.github.com>
2026-03-07 19:37:26 +0200

    added skeleton UI and dummy element. Need to set up event listners for user functionality

--- library.js
+++ library.js
@@ -7,15 +7,18 @@ function Book(title, author, pages, read) {
   this.title = title; 
   this.author = author; 
   this.pages = pages; 
-  this.read = read;
+  this.read = read === 'read';
   this.id = crypto.randomUUID(); 
   this.info = function() { 
    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`; 
   }; 
+}
+
+Book.prototype.toggleRead = function() { 
+  this.read = !this.read; 
 } 
 
 function addBookToLibrary(title, author, pages, read) { 
-
   const newBook = new Book(title, author, pages, read); 
 
   myLibrary.push(newBook); 
